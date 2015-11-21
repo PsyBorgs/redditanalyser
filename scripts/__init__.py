8170d8702a310832f140d2f103f7892d840cae88
Shane Martin <dev.sh@nemart.in>
2015-11-21 12:03:40 -0500

    Improve scraper to store comments in CSV

---
+++ scripts/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+import os.path
+
+PROJECT_DIR = os.path.abspath(os.path.join(__file__, '..', '..'))
+DATA_DIR = os.path.join(PROJECT_DIR, 'data')
--- scripts/comment_scraper.py
+++ scripts/comment_scraper.py
@@ -1,40 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 
 import praw
+import pandas as pd
 
-# Connect to Reddit
-# ----------------------------
+from . import DATA_DIR
 
-user_agent = "Quick comment thread scraper by /u/mediaarts"
-r = praw.Reddit(user_agent = user_agent)
+COMMENT_ATTRS = [
+    'id',
+    'created_utc',
+    'author',
+    'body',
+    'score',
+    'ups',
+    'downs',
+    'subreddit',
+    'subreddit_id',
+    'controversiality',
+    'is_root',
+    'parent_id',
+    'gilded',
+    'permalink',
+]
+SUBMISSION_ID = "1p1j6c"
 
+# connect to Reddit
+user_agent = "Comment thread scraper by /u/PsyBorgs"
+r = praw.Reddit(user_agent=user_agent)
 
-# Get comment thread and populate dict
-# ----------------------------
+# get comment thread and populate dict
+submission = r.get_submission(submission_id=SUBMISSION_ID)
+submission.replace_more_comments()
 
-submission_id = "1p1j6c"
-submission = r.get_submission(submission_id = submission_id, comment_sort = 'top')
-comments = submission.comments
+flat_comments = praw.helpers.flatten_tree(submission.comments)
+comments = []
+for c in flat_comments:
+    if isinstance(c, praw.objects.Comment):
+        comment = {}
+        for attr in COMMENT_ATTRS:
+            comment[attr] = getattr(c, attr)
+        comments.append(comment)
 
-flat_comments = praw.helpers.flatten_tree(comments)
-print("flat_comments length: {}".format(len(flat_comments)))
-print("flat_comments class: {}".format(type(flat_comments)))
-print("first comment class: {}".format(type(flat_comments[0])))
-print("last comment class: {}".format(type(flat_comments[len(flat_comments) - 1])))
-print("first comment attrs: {}".format(dir(flat_comments[0])))
-print("first comment score: {}".format(flat_comments[0].score))
-print("first comment author: {}".format(flat_comments[0].author))
-
-fname = submission_id + '.txt'
-with open(fname, 'w') as f:
-    for comment in flat_comments:
-        if isinstance(comment, praw.objects.Comment):
-            f.write("\n\n")
-            if comment.is_root:
-                f.write("---\n\n")
-            else:
-                f.write("Child comment \n")
-            f.write("Author: " + str(comment.author) + "\n")
-            f.write("Score: " + str(comment.score) + "\n")
-            f.write("Comment: \n\n" + comment.body.encode('utf-8'))
+# save comments
+comments_df = pd.DataFrame(comments, columns=COMMENT_ATTRS)
+csv_path = os.path.join(DATA_DIR, 'comments-{}.csv'.format(SUBMISSION_ID))
+comments_df.to_csv(csv_path, encoding='utf-8')

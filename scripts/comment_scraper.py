3d56e439feace9c0ac35ab66b5123f7560de0839
Shane Martin <dev.sh@nemart.in>
2015-12-24 18:20:50 -0500

    Refactor comment download method

--- scripts/comment_scraper.py
+++ scripts/comment_scraper.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import logging
 
 import praw
 import pandas as pd
 
 from . import DATA_DIR
+
+
+logger = logging.getLogger(__name__)
+
 
 COMMENT_ATTRS = [
     'id',
@@ -23,26 +28,38 @@ COMMENT_ATTRS = [
     'gilded',
     'permalink',
 ]
-SUBMISSION_ID = "1p1j6c"
+USER_AGENT = "Comment thread scraper by /u/PsyBorgs"
 
-# connect to Reddit
-user_agent = "Comment thread scraper by /u/PsyBorgs"
-r = praw.Reddit(user_agent=user_agent)
 
-# get comment thread and populate dict
-submission = r.get_submission(submission_id=SUBMISSION_ID)
-submission.replace_more_comments()
+def cache_submission(r, submission_id):
+    logger.debug("Downloading submission comments...")
+    submission = r.get_submission(submission_id=submission_id)
+    submission.replace_more_comments()
 
-flat_comments = praw.helpers.flatten_tree(submission.comments)
-comments = []
-for c in flat_comments:
-    if isinstance(c, praw.objects.Comment):
-        comment = {}
-        for attr in COMMENT_ATTRS:
-            comment[attr] = getattr(c, attr)
-        comments.append(comment)
+    logger.debug("Flattening and filtering comments...")
+    comments = []
+    for c in praw.helpers.flatten_tree(submission.comments):
+        if isinstance(c, praw.objects.Comment):
+            comment = {}
+            for attr in COMMENT_ATTRS:
+                comment[attr] = getattr(c, attr)
+            comments.append(comment)
 
-# save comments
-comments_df = pd.DataFrame(comments, columns=COMMENT_ATTRS)
-csv_path = os.path.join(DATA_DIR, 'comments-{}.csv'.format(SUBMISSION_ID))
-comments_df.to_csv(csv_path, encoding='utf-8')
+    logger.debug("Caching comments...")
+    comments_df = pd.DataFrame(comments, columns=COMMENT_ATTRS)
+    csv_path = os.path.join(DATA_DIR, 'comments', '{}.csv'.format(submission_id))
+    comments_df.to_csv(csv_path, encoding='utf-8')
+
+
+def main():
+    logging.basicConfig(level="DEBUG")
+
+    # connect to Reddit
+    r = praw.Reddit(user_agent=USER_AGENT)
+
+    submission_id = "dtg4j"
+    cache_submission(r, submission_id)
+
+
+if __name__ == '__main__':
+    main()

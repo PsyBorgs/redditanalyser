5d18f2d0c11dc35ee378a9fc56c3a24c4ddf6b1d
Shane Martin <dev.sh@nemart.in>
2016-01-27 11:09:20 -0500

    Add Submission model

--- redditanalysis/models.py
+++ redditanalysis/models.py
@@ -1,17 +1,28 @@
-from sqlalchemy import Column, Integer, String
-
 from redditanalysis.database import (
+    Base,
     db,
     Column,
     Model,
     ReferenceCol,
     relationship,
-    SurrogatePK,
+    SurrogatePK
 )
 
 
-Base = declarative_base()
+class Submission(Model, SurrogatePK):
+    __tablename__ = 'submissions'
 
+    created_utc = Column(db.Float, nullable=False)
+    fullname = Column(db.String, nullable=False)
+    subreddit = Column(db.String, nullable=False)
+    subreddit_id = Column(db.String, nullable=False)
+    permalink = Column(db.String, nullable=False)
 
-class Comment(Model):
-    __tablename__ = 'comments'
+    author = Column(db.String, nullable=False)
+    title = Column(db.Unicode, nullable=False)
+    selftext = Column(db.UnicodeText)
+
+    ups = Column(db.Integer, nullable=False)
+    downs = Column(db.Integer, nullable=False)
+    score = Column(db.Integer, nullable=False)
+    num_comments = Column(db.Integer, nullable=False)
